4f2c3e98e060f6774cd8757d99f4e22e2fb5fa57
sanjsndas <jaintipu65658@gmail.com>
2025-07-01 06:47:18 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import './style.css'
-import Home from './views/home'
-import NotFound from './views/not-found'
+import './style.css';
+import Home from './views/home';
+import About from './views/about';
+import Contact from './views/contact';
+import FAQ from './views/faq';
+import TermsAndConditions from './views/terms-and-conditions';
+import PrivacyPolicy from './views/privacy-policy';
+import CookiesPolicy from './views/cookies-policy';
+import NotFound from './views/not-found';
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route component={Home} exact path="/" />
-    <Route path="/privacy" element={<PrivacyPolicy />} />
-import TermsAndConditions from './views/terms-and-conditions';
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/terms" element={<TermsAndConditions />} />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
+        <Route path="/cookies" element={<CookiesPolicy />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-<Route path="/terms" element={<TermsAndConditions />} />
-
-        <Route component={NotFound} path="**" />
-    
-        <Redirect to="**" />
-      </Switch>
-    </Router>
-  )
-}
-
-ReactDOM.render(<App />, document.getElementById('app'))
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(<App />);
